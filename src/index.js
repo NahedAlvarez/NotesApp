e90a2a4014f410eba223669eaa4d9d3fa9acbcc2
Nahed David <nahed.alvarez@timba.co>
2020-11-17 22:52:38 -0500

    Se individualizaron las notas, se creo navbar, se creo un midleware authenticar las sessiones

--- src/index.js
+++ src/index.js
@@ -41,6 +41,8 @@ app.use((req,res,next)=>{
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg'); 
     res.locals.error = req.flash('error')
+    const _user = {...req.user}
+    res.locals.user =  _user._doc || null;
     next();
 });
 
